1c415bcd6f23765c9e79c9d1ebcfb4c4fde374e4
Purushothaman25 <90829353+Purushothaman25@users.noreply.github.com>
2021-09-16 17:11:47 +0530

    Create Program-2.c

---
+++ Program-2.c
@@ -0,0 +1,12 @@
+void main()
+{
+  int a,i,k=1;
+  printf("Enter a number"\n");
+         scanf("%d",&a);
+         for(i=0;i<a;i++)
+         {
+           printf("%d",k");
+                  k=k+2;
+                  }
+                  getch();
+                  }
