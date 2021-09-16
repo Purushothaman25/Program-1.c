bdade1691183859fbf3e66cfd26729d5874c0818
Purushothaman25 <90829353+Purushothaman25@users.noreply.github.com>
2021-09-16 17:31:12 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,10 +1,12 @@
 void main()
 {
   double a,b,add,sub,mul,div;
+  printf("Enter the numbers");
+  scanf("%d %d",&a,&b);
   add=a+b;
   sub=a-b;
   mul=a*b;
   div=a/b;
-  printf("%d %d %d %d",&add, &sub, &mul,&div);
+  printf("%d %d %d %d",add, sub, mul,div);
   getch();
 }
